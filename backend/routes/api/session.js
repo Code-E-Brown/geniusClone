e76d9c633b5ebea1aa8204c33ffbb85c1c17dbe2
cody <cody.brown1993@gmail.com>
2021-06-17 16:22:41 -0400

    logout route complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,5 +31,14 @@ router.post(
 );
 
 
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
+
 
 module.exports = router;
